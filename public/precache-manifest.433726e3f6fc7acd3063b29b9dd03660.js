0774ca485d36a6780e6a28c7817916faf885b578
Argali <arkharovp@gmail.com>
2021-05-14 22:43:09 +0300

    trying to fix ruined structure issue

---
+++ public/precache-manifest.433726e3f6fc7acd3063b29b9dd03660.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a823693130d500abedf3dda0b253a132",
+    "url": "/index.html"
+  },
+  {
+    "revision": "403d47921c1be72ea66c",
+    "url": "/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "33a092d6e56d8260b38a",
+    "url": "/static/css/main.d3755980.chunk.css"
+  },
+  {
+    "revision": "403d47921c1be72ea66c",
+    "url": "/static/js/2.d1a8bfd2.chunk.js"
+  },
+  {
+    "revision": "774152ec3dfb5dadc6880f2e7f79e400",
+    "url": "/static/js/2.d1a8bfd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33a092d6e56d8260b38a",
+    "url": "/static/js/main.38317bc3.chunk.js"
+  },
+  {
+    "revision": "74332159e2d41240d9a0",
+    "url": "/static/js/runtime-main.b4087ef2.js"
+  }
+]);
